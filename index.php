36fb7e2f2e0cc839ae0049143b9052fa62960ba8
Alana Silva <102611146+AlanaSilva@users.noreply.github.com>
2022-04-08 11:37:23 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>Meu priemeiro site em PHP</title>
+	</head>
+
+	<body>
+
+		<?php
+			for ( $i = 0 ; $i < 10 ; $i++ ) {
+				print("linha número" . $i . "<br/>");
+			}
+		?>
+
+	</body>
+
+<html>
